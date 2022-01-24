838a8ed03d4b1d33f6e8598dc9a7e99e904127e4
Hasan Sattar <hasanraza1798@gmail.com>
2022-01-24 15:16:18 +0000

    added more pages and updating job reulst page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CompanyJobs from './components/CompanyJobs'
 import Home from './components/Home'
+import JobDetails from './components/JobDetails'
 import JobResults from './components/JobResults'
 import Navbar from './components/Navbar'
 
@@ -10,6 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/jobs" element={ <JobResults /> } />
+        <Route path="/jobs:/jobId" element={ <JobDetails /> } />
+        <Route path="/company/:companyName" element={ <CompanyJobs /> } />
       </Routes>
     </Router>
   )
--- src/components/JobResults.jsx
+++ src/components/JobResults.jsx
@@ -9,12 +9,14 @@ export default function JobResults() {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
-  const [limit, setLimit] = useState(25)
+  const [limit, setLimit] = useState(24)
   const [page, setPage] = useState(1)
 
   const [searchParams, setSearchParams] = useSearchParams()
 
-  const { data: jobData, loading: jobLoading, error: jobError, fetchData } = useFetch(`query=${searchParams.get('query')}&limit=${limit}`)
+  const params = searchParams.get('query') ? `query=${searchParams.get('query')}&limit=${limit}` : `limit=${limit}`
+
+  const { data: jobData, loading: jobLoading, error: jobError, fetchData } = useFetch(params)
 
   useEffect(() => {
     setData(jobData)
@@ -23,13 +25,13 @@ export default function JobResults() {
   }, [jobData, jobLoading, jobError])
 
   return (
-    <Container maxWidth="xl" style={{ marginTop: '3rem'}}>
+    <Container maxWidth="xl" style={{ margin: '3rem 0'}}>
       <h2>Showing results for {searchParams.get('query')}</h2>
       <Grid container spacing={2} style={{ marginTop: '0.5rem'}}>
       {
         data && data.map(job => (
           <Grid item  key={job._id} xs={12} md={6}>
-            <SingleJob />
+            <SingleJob job={job} />
           </Grid>
         ))
       }
